ba26bc342d1214396a6fa09eba1711e78dfadf79
David Matousch <david.matousch@tractive.com>
2024-07-07 11:00:15 +0200

    export single modules for easy use

--- rollup.config.js
+++ rollup.config.js
@@ -4,37 +4,74 @@ import typescript from '@rollup/plugin-typescript';
 import { babel } from '@rollup/plugin-babel';
 import scss from 'rollup-plugin-scss';
 
-export default {
-    input: 'src/index.ts',
-    output: [
-        {
-            file: 'dist/silkify.cjs.js',
-            format: 'cjs',
+export default [
+    {
+        input: 'src/animations/scroll-trigger.ts',
+        output: {
+            file: 'dist/animations/scroll-trigger.js',
+            format: 'esm',
         },
-        {
+        plugins: [
+            resolve(),
+            commonjs(),
+            typescript({
+                tsconfig: './tsconfig.json',
+            }),
+            babel({
+                babelHelpers: 'bundled',
+                exclude: 'node_modules/**',
+                presets: ['@babel/preset-env'],
+            }),
+            scss({
+                fileName: 'scroll-trigger.css',
+                outputStyle: 'compressed',
+            }),
+        ],
+    },
+    {
+        input: 'src/animations/side-scroll.ts',
+        output: {
+            file: 'dist/animations/side-scroll.js',
+            format: 'esm',
+        },
+        plugins: [
+            resolve(),
+            commonjs(),
+            typescript({
+                tsconfig: './tsconfig.json',
+            }),
+            babel({
+                babelHelpers: 'bundled',
+                exclude: 'node_modules/**',
+                presets: ['@babel/preset-env'],
+            }),
+            scss({
+                fileName: 'side-scroll.css',
+                outputStyle: 'compressed',
+            }),
+        ],
+    },
+    {
+        input: 'src/index.ts',
+        output: {
             file: 'dist/silkify.esm.js',
             format: 'esm',
         },
-        {
-            file: 'dist/silkify.umd.js',
-            format: 'umd',
-            name: 'Silkify',
-        },
-    ],
-    plugins: [
-        resolve(),
-        commonjs(),
-        typescript({
-            tsconfig: './tsconfig.json',
-        }),
-        babel({
-            babelHelpers: 'bundled',
-            exclude: 'node_modules/**',
-            presets: ['@babel/preset-env'],
-        }),
-        scss({
-            fileName: 'silkify.css',
-            outputStyle: 'compressed',
-        }),
-    ],
-};
+        plugins: [
+            resolve(),
+            commonjs(),
+            typescript({
+                tsconfig: './tsconfig.json',
+            }),
+            babel({
+                babelHelpers: 'bundled',
+                exclude: 'node_modules/**',
+                presets: ['@babel/preset-env'],
+            }),
+            scss({
+                fileName: 'silkify.css',
+                outputStyle: 'compressed',
+            }),
+        ],
+    },
+];
