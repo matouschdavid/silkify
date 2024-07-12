e2336abf1373ebb691b2e5c1bc118e56cd7f8d6f
matouschdavid <42933956+matouschdavid@users.noreply.github.com>
2024-07-12 12:46:33 +0000

    add v2 of side-scroll

--- src/side-scroll.ts
+++ src/side-scroll.ts
@@ -1,25 +1,54 @@
 import "./styles/side-scroll.scss";
 
-function calculateAnimationTimings(): void {
-    const horizontalScroller = document.querySelectorAll<HTMLElement>(".horizontal-scroll");
+const scrollContainers = [] as HTMLElement[];
 
-    horizontalScroller.forEach((scroller) => {
-        const scrollSpeed = parseFloat(scroller.dataset.scrollSpeed!) || 1;
-
-        const offsetTop = scroller.offsetTop;
-        const contentHeight = scroller.children[0].clientHeight;
-        const height =
-            (contentHeight + window.innerHeight) * scrollSpeed - contentHeight;
-        scroller.style.setProperty("--scroll-start", `${offsetTop}px`);
-        scroller.style.setProperty("--scroll-end", `${offsetTop + height}px`);
-
-        scroller.style.setProperty("--content-height", `${contentHeight}px`);
-        scroller.style.setProperty("--scroll-speed", scrollSpeed.toString());
-    });
-}
-
-calculateAnimationTimings();
+processSideScroll();
 
 window.addEventListener("resize", () => {
-    calculateAnimationTimings();
+  scrollContainers.forEach((element) => {
+    calculateAnimationTiming(element);
+  });
 });
+
+function processSideScroll(): void {
+  const elementsToAnimate =
+    document.querySelectorAll<HTMLElement>("[scroll-direction]");
+  elementsToAnimate.forEach((element) => {
+    const direction = element.dataset.scrollDirection!;
+    const scrollSpeed = parseInt(element.getAttribute("scroll-speed")!) || 1;
+
+    const scrollContainer = document.createElement("div");
+    scrollContainer.classList.add("horizontal-scroll");
+    scrollContainer.classList.add(`to-${direction}`);
+    scrollContainer.dataset.scrollSpeed = scrollSpeed.toString();
+
+    element.removeAttribute("scroll-direction");
+    element.removeAttribute("scroll-speed");
+
+    const innerContainer = document.createElement("div");
+    innerContainer.classList.add("horizontal-scroll-inner");
+    innerContainer.appendChild(element);
+
+    scrollContainer.appendChild(innerContainer);
+
+    element.parentElement!.replaceChild(scrollContainer, element);
+
+    calculateAnimationTiming(scrollContainer);
+
+    scrollContainers.push(scrollContainer);
+  });
+}
+
+function calculateAnimationTiming(element: HTMLElement): void {
+  const scrollSpeed = parseInt(element.getAttribute("scroll-speed")!) || 1;
+
+  const offsetTop = element.offsetTop;
+  const contentHeight = element.children[0].clientHeight;
+  const height =
+    (contentHeight + window.innerHeight) * scrollSpeed - contentHeight;
+  element.style.setProperty("--scroll-start", `${offsetTop}px`);
+  element.style.setProperty("--scroll-end", `${offsetTop + height}px`);
+
+  element.style.setProperty("--content-height", `${contentHeight}px`);
+  element.style.setProperty("--scroll-speed", scrollSpeed.toString());
+}
